8ea83660441c18f39df6234bfb8171593344dbc2
Nick Adams <nick.pappas.adams@gmail.com>
2023-05-21 17:59:27 -0500

    added balanced bracets

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,44 @@
 "use strict";
 // number type
 let id = 5;
-console.log("ID:", id);
 // string type
 let myName = "Nick";
-console.log(myName);
 // splitting the string into an array
 let splitStr = myName.split("");
 // looping through the split string array
-for (let ch of splitStr) {
-    console.log(ch);
-}
+// for (let ch of splitStr) {
+//   console.log(ch);
+// }
 // boolean type
 let isPublished = true;
-console.log(isPublished);
+//console.log(isPublished);
 // array type
 let nums = [1, 2, 3, 4, 5];
-console.log(nums);
+//console.log(nums);
 // array any type
 let myArr = [1, true, "hello"];
 // tuple
 let person = [1, "nick", true];
 // union
 let pid = 2;
-pid = '5';
+const balancedBrackets = (str) => {
+    if (str.length % 2 !== 0)
+        return false;
+    let myArray = [];
+    let splitStr = str.split("");
+    for (let c of splitStr) {
+        let lastIndex = myArray[myArray.length - 1];
+        if (c === "{" || c == "(" || c == "[")
+            myArray.push(c);
+        else if (c == "}" && lastIndex == "{")
+            myArray.pop();
+        else if (c == ")" && lastIndex == "(")
+            myArray.pop();
+        else if (c == "]" && lastIndex == "[")
+            myArray.pop();
+    }
+    if (myArray.length === 0)
+        return true;
+    return false;
+};
+console.log(balancedBrackets("(){}"));
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 // number type
 let id: number = 5;
-console.log("ID:", id);
 
 // string type
 let myName: string = "Nick";
-console.log(myName);
+
 // splitting the string into an array
 let splitStr: string[] = myName.split("");
 // looping through the split string array
-for (let ch of splitStr) {
-  console.log(ch);
-}
+// for (let ch of splitStr) {
+//   console.log(ch);
+// }
 // boolean type
 let isPublished: boolean = true;
-console.log(isPublished);
+//console.log(isPublished);
 
 // array type
 let nums: number[] = [1, 2, 3, 4, 5];
-console.log(nums);
+//console.log(nums);
 
 // array any type
 let myArr: any[] = [1, true, "hello"];
@@ -27,4 +26,22 @@ let person: [number, string, boolean] = [1, "nick", true];
 
 // union
 let pid: number | string = 2;
-pid = '5'
+
+const balancedBrackets = (str: string): boolean => {
+  if (str.length % 2 !== 0) return false;
+  let myArray: string[] = [];
+  let splitStr: string[] = str.split("");
+
+  for (let c of splitStr) {
+    let lastIndex = myArray[myArray.length - 1];
+    if (c === "{" || c == "(" || c == "[") myArray.push(c);
+    else if (c == "}" && lastIndex == "{") myArray.pop();
+    else if (c == ")" && lastIndex == "(") myArray.pop();
+    else if (c == "]" && lastIndex == "[") myArray.pop();
+  }
+
+  if (myArray.length === 0) return true;
+  return false;
+};
+
+console.log(balancedBrackets("(){}"));
